1a64565886b1256d9d0570e39d85625f413402f5
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-06-11 10:24:07 +0100

    steps , navigation

--- src/Forms/MultiForm.js
+++ src/Forms/MultiForm.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import Navigation from "./Navigation";
+import { Steps, useSteps } from "react-step-builder";
+import Step1 from "../Steps/Step1";
+import Step2 from "../Steps/Step2";
+import Step3 from "../Steps/Step3";
+import Step4 from "../Steps/Step4";
+import StepFinal from "../Steps/StepFinal"
+import Container from "react-bootstrap/Container";
 
 const MultiForm = () => {
-    return (
-        <div>
-            MultiForm
-        </div>
-    );
-}
+  const { prev, next, total, current } = useSteps();
+
+  return (
+    <Container
+      style={{ width: "28rem", border: "1px solid #ddd", padding: "10px" }}
+    >
+      <Steps>
+        <Step1 />
+        <Step2 />
+        <Step3 />
+        <Step4 />
+        <StepFinal/>
+      </Steps>
+      <div>
+      <Navigation prev={prev} next={next}  />
+      </div>
+    </Container>
+  );
+};
 
 export default MultiForm;
--- src/Forms/Navigation.js
+++ src/Forms/Navigation.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-const Navigation = () => {
-    return (
-        <div>
-            Navigation
-        </div>
-    );
-}
+const Navigation = ({ prev, next }) => {
+  return (
+    <div>
+      <Container style={{ padding: "20px" }}>
+        <Row>
+          <Col className="d-flex justify-content-center">
+            <Button
+              variant="primary"
+              onClick={prev}
+              style={{ marginRight: 10 }}
+            >
+              {" "}
+              Previous
+            </Button>
+          </Col>
+          <Col className="d-flex justify-content-center">
+            {" "}
+            <Button
+              variant="primary"
+              onClick={next}
+              style={{ marginRight: 10 }}
+            >
+              Next
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Navigation;
