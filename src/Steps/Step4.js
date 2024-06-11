b7f63be3d07aa3fe82553b2c887b93e453d33846
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-06-11 14:08:21 +0100

    Update

--- src/Steps/Step4.js
+++ src/Steps/Step4.js
@@ -7,22 +7,26 @@ const Step4 = ({ state }) => {
       <h2>Review Form</h2>
       <hr />
       <p>
-        {" "}
-        Name: {state.fname} {state.lname}{" "}
+        Name: {state.fname ? `${state.fname}` : <span> No Name input </span>}{" "}
+       
       </p>
-      <p> Email: {state.email} </p>
+<p> Last Name : {state.lname ? `${state.lname}` : <span> No Last Name input </span>}</p>
+      <p>
+        {" "}
+        Email: {state.email ?  `${state.email}` : <span> No email input</span>}{" "}
+      </p>
       <p> Start Date: {moment(state.startDate).format("DD-MM-YYYY")}</p>
       <p>End Date: {moment(state.endDate).format("DD-MM-YYYY")}</p>
       <p>
-        Price: Adult £{state.price}, Child £{state.childprice}
+        Price: Adult £ {state.price ? `${state.price}` : <span>No Adult Price </span>}, Child £ {state.childprice ? `${state.childprice}` : <span> No Child Price </span>}
       </p>
       <p>Currency: {state.currency}</p>
-      <p>Ticket Option : {state.name} </p>
+      <p>Ticket Option :{state.name ? `${state.name}` : <span>No Day, Week or Month Selected </span>}</p>
 
       <p>Status : {state.isStatus ? "Published" : "Unpublished"}</p>
       <p>
-        Ticket Activity: Adult - {state.activationType} , Child -
-        {state.activationTypeChild}
+        Ticket Activity: Adult - {state.activationType ? `${state.activationType}` : <span> No Adult Activation </span>} , Child -
+        {state.activationTypeChild ? `${state.activationTypeChild}` : <span> No Child Activation </span>} 
       </p>
     </div>
   );
