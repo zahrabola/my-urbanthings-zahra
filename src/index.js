80ed5773ee42c089e1318e1358e97d250e92de67
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-06-11 10:09:43 +0100

    import

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StepsProvider } from "react-step-builder";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
+  <StepsProvider>
     <App />
-  </React.StrictMode>
+  </StepsProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
